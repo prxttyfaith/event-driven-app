41eb5192f4e3e2af9b7e3b3dfd810e213d4a7836
Pretty Faith <pfimisuarez@addu.edu.ph>
2024-02-23 00:41:24 +0800

    added style

--- src/app.js
+++ src/app.js
@@ -4,9 +4,19 @@ const App = () => {
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
+    console.log('Fetching tasks from API...');
     fetch('http://localhost:3000/to-do-list')
-      .then(response => response.json())
-      .then(data => setTasks(data.data))
+      .then(response => {
+        console.log('API Response:', response);
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('API Data:', data);
+        setTasks(data.data);
+      })
       .catch(error => console.error('Error fetching data:', error));
   }, []);
 
